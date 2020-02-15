30b8d34a035217caa71e2bf2a295936077f275cb
NiceSlice <tanjamiskovic1@gmail.com>
2020-02-15 13:59:12 +0200

    Updates

---
+++ precache-manifest.810a0946ffe63b0909fdc61baa13be4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c2ee32f3286aa3cfe49a555795ad0b",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "ad8c467a366c6295b16b",
+    "url": "/sudoku/static/css/main.f07a34eb.chunk.css"
+  },
+  {
+    "revision": "9508d0e08a64a1418238",
+    "url": "/sudoku/static/js/2.885ab30f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sudoku/static/js/2.885ab30f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad8c467a366c6295b16b",
+    "url": "/sudoku/static/js/main.1f7b1541.chunk.js"
+  },
+  {
+    "revision": "c4a6c89a504370d394ff",
+    "url": "/sudoku/static/js/runtime-main.efde695e.js"
+  }
+]);
